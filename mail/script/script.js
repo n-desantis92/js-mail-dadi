86bfaf4d5f716d587f58c08c9e37beaa160fd23e
n-desantis92 <dsnwebdev@gmail.com>
2021-03-01 16:16:33 +0100

    aggiunto ciclo per mail

---
+++ mail/script/script.js
@@ -0,0 +1,19 @@
+// Chiedi all’utente la sua email,
+var mail = prompt("inserisci e-mail");
+var autorizzazione = false;
+// controlla che sia nella lista di chi può accedere,
+var accessi = ["giggi@gmail","luca@gmail","paolo@gmail","marco@gmail","michele@gmail"];
+var i;
+for(i = accessi.lenght; i > 0 ; i--){
+  // confronto accessi[i] con mail
+  if(accessi[i] == mail){
+    autorizzazione = true;
+  }
+}
+// stampa un messaggio appropriato sull’esito del controllo
+if(autorizzazione) {
+  alert("ben fatto ora puoi accedere");
+}
+else{
+  alert("inserisci un e-mail valida");
+}
